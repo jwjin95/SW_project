5ddf4696c1f3af33d5e5378b667fbd3699662047
leedhn <leedhn@yonsei.ac.kr>
2020-11-20 17:55:38 +0900

    프로젝트 수정중-Slider

--- lib/main_DH.dart
+++ lib/main_DH.dart
@@ -2,10 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:outline/Q1.dart';
 import 'package:outline/Q4.dart';
-
 //constants
-const kPrimaryColor = Color(0xff186049);
-const kBorderColor = Color(0xffE4D5C7);
+const kPrimaryColor = Color(0xff9ce6ff);
+const kBorderColor = Color(0xfff2feff);
 const kBackgroundWhiteColor = Color(0xffF8F6F5);
 
 const kAmounts = ["50", "100", "250", "500"];
@@ -15,6 +14,18 @@ const kTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
 );
 //
+// //constants
+// const kPrimaryColor = Color(0xff186049);
+// const kBorderColor = Color(0xffE4D5C7);
+// const kBackgroundWhiteColor = Color(0xffF8F6F5);
+//
+// const kAmounts = ["50", "100", "250", "500"];
+//
+//
+// const kTextStyle = TextStyle(
+//   fontWeight: FontWeight.bold,
+// );
+// //
 
 void main(){
   runApp(paymentsApp());
